6f8d8f23a67c5f2585ee8f9ef73621e8b220d559
sartain <alex.sartain@zuhlke.com>
2021-09-29 15:50:34 +0100

    Test to pass example

--- minesweeperTest.py
+++ minesweeperTest.py
@@ -4,7 +4,7 @@ import unittest
 class MinesweeperTest(unittest.TestCase):
 
     def setUp(self):
-        self.fourByFourGame = "4 4\n*...\n....\n.*..\n...."
+        self.fourByFourGame = "*...\n....\n.*..\n...."
 
     def testNumberOfMines(self):
         self.assertEquals(2, Minesweeper().getMines(self.fourByFourGame))
@@ -17,4 +17,7 @@ class MinesweeperTest(unittest.TestCase):
 
     def testMineBelowSurrounding(self):
         self.assertEquals("111\n1*1", Minesweeper().convertRowToOutput("...\n.*."))
+
+    def testExample(self):
+        self.assertEquals("*100\n2210\n1*10\n1110", Minesweeper().convertRowToOutput(self.fourByFourGame))
 unittest.main()
